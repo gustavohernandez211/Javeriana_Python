8490c3ace5cd7d4f1a03cf48c553fbb6e752b2f0
gustavohernandez211 <110845032+gustavohernandez211@users.noreply.github.com>
2022-08-10 11:03:32 -0500

    Add files via upload
    Programacion - actividad

---
+++ 0x005-Functions.py.py
@@ -0,0 +1,9 @@
+def sumas(sumando1,sumando2 ):
+  print("resultado",sumando1+sumando2)
+  
+def resta(minuendo, sustraendo):
+  print("resultado",minuendo-sustraendo)
+def multiplicacion(producto1,producto2):
+  print("resultado",producto1*producto2)
+def potenciacion(base,exponente):
+  print("resultado",base**exponente)
