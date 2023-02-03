0fd5fd2ec6f0fd9761b7829282debecfb4b1be45
gustavohernandez211 <110845032+gustavohernandez211@users.noreply.github.com>
2023-02-03 12:36:15 -0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-def sumas(sumando1,sumando2 ):
-  print("resultado",sumando1+sumando2)
-  
-def resta(minuendo, sustraendo):
-  print("resultado",minuendo-sustraendo)
-def multiplicacion(producto1,producto2):
-  print("resultado",producto1*producto2)
-def potenciacion(base,exponente):
-  print("resultado",base**exponente)
